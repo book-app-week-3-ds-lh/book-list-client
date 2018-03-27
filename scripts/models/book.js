b039548fca969032d9b9fd8793f227b32064676e
danielvportlandia <danielshelton90@gmail.com>
2018-03-27 16:26:01 -0700

    book.js and bookView.js almost finished.

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,40 +1,38 @@
 'use strict';
 
 var app = app || {};
+$.getJSON('http://localhost:3000/api/v1/books').then( results => console.log(results));
 
 (function(module){
 
-$.getJSON('http://localhost:3000/api/v1/books').then( results => console.log(results));
 
-function Book (bookDataObj) {
-    this.title = bookDataObj.title;
-    this.author = bookDataObj.author;
-    this.isbn = bookDataObj.isbn;
-    this.image_url = bookDataObj.image_url;
-    this.description = bookDataObj.description
-}
+  function Book(bookDataObj) {
+    Object.keys(bookDataObj).forEach(key => {
+      this[key] = bookDataObj[key];
+    }, this);
+  }
 
-Book.all = [];
+  Book.all = [];
 
-Book.prototype.toHTML = function() {
+  Book.prototype.toHTML = function() {
     let template = Handlebars.compile($('#book-list-template').text());
     return template(this);
-};
+  };
 
-Book.loadAll = bookData => {
+  Book.loadAll = bookData => {
     bookData.sort((a, b) => (a.title) - (b.title));
     Book.all = bookData.map(x => new Book(x));
     return Book.all;
   };
 
   Book.fetchAll = callback => {
-    $.get('/api/v1/books')
+    $.getJSON('http://localhost:3000/api/v1/books')
       .then(results => {
         Book.loadAll(results);
         callback();
       })
-      //on failure needs errorCallback (step 9)
+      .catch(app.errorView.initErrorPage);
   };
 
-module.Book = Book;
+  module.Book = Book;
 })(app);
